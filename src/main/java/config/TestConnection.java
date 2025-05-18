c7854b68e05d01259cd640ccd172f8869ce64206
Hoàng Kim Oanh <hoangkimoanh04052004@gmail.com>
2025-05-19 00:01:24 +0700

    oanh

--- src/main/java/config/TestConnection.java
+++ src/main/java/config/TestConnection.java
@@ -16,4 +16,17 @@ public class TestConnection {
             e.printStackTrace();
         }
     }
+
+	public static void main(String[] args) {
+	    try (Connection conn = DBConnection.getConnection()) {
+	        if (conn != null) {
+	            System.out.println("Kết nối đến cơ sở dữ liệu thành công!");
+	        } else {
+	            System.out.println("Kết nối thất bại!");
+	        }
+	    } catch (SQLException e) {
+	        System.out.println("Lỗi kết nối: " + e.getMessage());
+	        e.printStackTrace();
+	    }
+	}
 }
