5b7c3a2f5cf4fbe792ec4193474e4a6e4e6226a7
nak957 <thequyen700@gmail.com>
2025-06-18 00:16:00 +0700

    QLkhachhang

--- src/main/java/model/NguoiDung.java
+++ src/main/java/model/NguoiDung.java
@@ -3,40 +3,30 @@ package model;
 import java.sql.Timestamp;
 
 public class NguoiDung {
-    private int maNguoiDung;       // Đổi từ id -> maNguoiDung
+    private int maNguoiDung;
     private String email;
     private String matKhau;
     private String vaiTro;
     private String hoTen;
     private String soDienThoai;
+    private String diaChi;
     private Timestamp ngayTao;
     private Timestamp ngayCapNhat;
 
-    // Constructor mặc định
     public NguoiDung() {}
 
-    // Constructor đầy đủ
-    public NguoiDung(int maNguoiDung, String email, String matKhau, String vaiTro, String hoTen, Timestamp ngayTao, Timestamp ngayCapNhat) {
+    public NguoiDung(int maNguoiDung, String email, String matKhau, String vaiTro, String hoTen, String soDienThoai, String diaChi, Timestamp ngayTao, Timestamp ngayCapNhat) {
         this.maNguoiDung = maNguoiDung;
         this.email = email;
         this.matKhau = matKhau;
         this.vaiTro = vaiTro;
         this.hoTen = hoTen;
         this.soDienThoai = soDienThoai;
+        this.diaChi = diaChi;
         this.ngayTao = ngayTao;
         this.ngayCapNhat = ngayCapNhat;
     }
 
-    // Constructor không có ID, dành cho insert mới
-    public NguoiDung(String email, String matKhau, String vaiTro, String hoTen) {
-        this.email = email;
-        this.matKhau = matKhau;
-        this.vaiTro = vaiTro;
-        this.hoTen = hoTen;
-        this.soDienThoai = soDienThoai;
-    }
-
-    // Getter - Setter
     public int getMaNguoiDung() {
         return maNguoiDung;
     }
@@ -77,6 +67,22 @@ public class NguoiDung {
         this.hoTen = hoTen;
     }
 
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
     public Timestamp getNgayTao() {
         return ngayTao;
     }
@@ -92,12 +98,4 @@ public class NguoiDung {
     public void setNgayCapNhat(Timestamp ngayCapNhat) {
         this.ngayCapNhat = ngayCapNhat;
     }
-    
-    public String getSoDienThoai() {
-    	return soDienThoai;
-    }
-    
-	public void setSoDienThoai(String soDienThoai) {
-		this.soDienThoai = soDienThoai;
-	}
 }
